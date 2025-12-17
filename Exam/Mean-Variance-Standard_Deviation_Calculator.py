55a367b9810ee6407015e7169cedfb330980535e
PhyscØ <105894521+AudiForze@users.noreply.github.com>
2025-12-17 16:25:30 -0300

    Add files via upload

---
+++ Exam/Mean-Variance-Standard_Deviation_Calculator.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+def calculate(list_of_numbers):
+    if len(list_of_numbers) != 9:
+        raise ValueError("List must contain nine numbers.")
+    arr = np.array(list_of_numbers).reshape(3, 3)
+    
+    result = {
+        'mean': [
+            np.mean(arr, axis=0).tolist(),   
+            np.mean(arr, axis=1).tolist(),   
+            float(np.mean(arr))              
+        ],
+        'variance': [
+            np.var(arr, axis=0).tolist(),
+            np.var(arr, axis=1).tolist(),
+            float(np.var(arr))
+        ],
+        'standard deviation': [
+            np.std(arr, axis=0).tolist(),
+            np.std(arr, axis=1).tolist(),
+            float(np.std(arr))
+        ],
+        'max': [
+            np.max(arr, axis=0).tolist(),
+            np.max(arr, axis=1).tolist(),
+            int(np.max(arr))
+        ],
+        'min': [
+            np.min(arr, axis=0).tolist(),
+            np.min(arr, axis=1).tolist(),
+            int(np.min(arr))
+        ],
+        'sum': [
+            np.sum(arr, axis=0).tolist(),
+            np.sum(arr, axis=1).tolist(),
+            int(np.sum(arr))
+        ]
+    }
+    return result
